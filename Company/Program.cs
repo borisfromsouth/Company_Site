1190de2bacba9c81f8022d91205a1b41d9831873
boris_from_south <sashaborisenko@tut.by>
2022-09-26 17:15:55 +0300

    Upload Project
    Upload Project

--- Company/Program.cs
+++ Company/Program.cs
@@ -1,20 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// добавляем сервисы
 builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseDeveloperExceptionPage();
 }
+
 app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
+app.UseEndpoints(endpoints => 
+{ 
+    endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/");
+}): 
+//app.UseAuthorization();
 
 app.MapRazorPages();
 
